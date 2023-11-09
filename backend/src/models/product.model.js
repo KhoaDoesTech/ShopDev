16c965dce8da2725f54c3d30438c50b3adc8ab6f
Anh Khoa <khoahoc72@gmail.com>
2023-11-09 13:04:07 +0700

    Update product model

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,30 +1,45 @@
 "use strict";
 
 const { Schema, model } = require("mongoose"); // Erase if already required
-
+const slugify = require("slugify");
 const DOCUMENT_NAME = "Product";
 const COLLECTION_NAME = "Products";
 
 // Declare the Schema of the Mongo model
 const productSchema = new Schema(
   {
-    product_name: { type: String, required: true },
+    product_name: { type: String, required: true }, // quan jean cao cap
     product_thumb: { type: String, required: true },
     product_description: String,
+    product_slug: String, // quan-jean-cao-cap
     product_price: { type: Number, require: true },
     product_quantity: { type: Number, require: true },
     product_type: { type: String, require: true, enum: ["Electronics", "Clothing", "Furniture"] },
     product_shop: { type: Schema.Types.ObjectId, ref: "Shop" },
     product_attributes: { type: Schema.Types.Mixed, required: true },
+    product_ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    product_variations: { type: Array, default: [] },
+    isDraft: { type: Boolean, default: true, index: true, select: false },
+    isPublished: { type: Boolean, default: false, index: true, select: false },
   },
   {
     collection: COLLECTION_NAME,
     timestamps: true,
   }
 );
-/**
- * Áo (size, brand, màu)
- */
+
+// Document middleware: runs before save() and create()
+productSchema.pre("save", function (next) {
+  this.product_slug = slugify(this.product_name, { lower: true });
+  next();
+});
+
 // define the product type = clothing
 const clothingSchema = new Schema(
   {
