770ce6732b915ffc8048094b9dd7069c5f9700e2
Anh Khoa <khoahoc72@gmail.com>
2023-12-06 10:17:18 +0700

    fix bug

--- backend/src/services/redis.service.js
+++ backend/src/services/redis.service.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const { resolve } = require("path");
 const redis = require("redis");
 const { promisify } = require("util");
 const { reservationInventory } = require("../models/repositories/inventory.repo");
 const redisClient = redis.createClient();
 
-const pexpire = promisify(redisClient.pexpire).bind(redisClient);
-const setnxAsync = promisify(redisClient.setnx).bind(redisClient);
+const pexpire = promisify(redisClient.pExpire).bind(redisClient);
+const setnxAsync = promisify(redisClient.setNX).bind(redisClient);
+const delAsyncKey = promisify(redisClient.del).bind(redisClient);
 
 const acquireLock = async (productId, quantity, cartId) => {
   const key = `lock_v2023${productId}`;
@@ -36,8 +36,6 @@ const acquireLock = async (productId, quantity, cartId) => {
 };
 
 const releaseLock = async (keyLock) => {
-  const delAsyncKey = promisify(redisClient.del).bind(redisClient);
-
   return await delAsyncKey(keyLock);
 };
 
